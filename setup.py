543d5a7a7af9cc4c3ac61e3905781cc81a3da9ab
Ari Makela <hauva@iki.fi>
2012-08-13 16:06:09 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+    name='python-ao-libs',
+    author='Ari Makela',
+    author_email='hauva@iki.fi',
+    url='https://github.com/hauva69/python-ao-libs',
+    version='1.0',
+    packages=['ao',],
+    long_description=open('README').read(),
+)
+
